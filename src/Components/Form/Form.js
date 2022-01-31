4a495994f1de60ac5e6a8fd1385e3ab96475197c
Rabi-Mezan <ravimezan@gmail.com>
2022-01-31 23:44:44 +0600

    calender added

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import bgimg from '../../img/flightbg.jpg'
+import Departure from '../Departure/Departure';
 import From from '../From/From';
 import RadioButtons from '../RadioButtons/RadioButtons';
+import Return from '../Return/Return';
 import To from '../To/To';
 import './Form.css'
 
@@ -20,14 +22,21 @@ const Form = () => {
                 <h1 className='fs-2 mb-5 mt-3 fw-light text-warning ms-5'>Flight deals with Virgin Atlantic</h1>
                 <RadioButtons></RadioButtons>
 
-                <div className='d-flex justify-content-center align-items-center w-100'>
-                    <div class="row row-cols-2">
-                        <div class="col d-flex">
+                <div className='d-flex justify-content-around align-items-center w-100'>
+                    <div class="row d-flex align-items-center">
+                        <div class="col-lg-8 d-flex">
                             <From></From>
                             <To></To>
                         </div>
 
-                        <div class="col">Column</div>
+                        <div class="col-lg-4 row">
+                            <div className='col-lg-6'>
+                                <Departure></Departure>
+                            </div>
+                            <div className='col-lg-6'>
+                                <Return></Return>
+                            </div>
+                        </div>
 
                     </div>
 
